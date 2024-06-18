c9e1794ff4d50aa6126500212693428d0541ae91
Mitta Shireesha <mittashireesha@Mittas-Laptop.local>
2024-06-18 23:17:20 +0530

    Cardinalities

--- src/main/java/com/scaler/demo/Model/Category.java
+++ src/main/java/com/scaler/demo/Model/Category.java
@@ -3,9 +3,12 @@ package com.scaler.demo.Model;
 import com.scaler.demo.dto.CategoryResponseDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,6 +16,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Category  extends BaseModel{
      private String name;
+
+     @OneToMany(mappedBy = "category") // we give mapped by to avoid duplicate relations
+     //else it creates separate mapping table for category product combination
+     // as we have already specified cardinality in Product class
+     private List<Product> products;
 
      public CategoryResponseDTO convertToResponseDTO() {
           CategoryResponseDTO categoryResponseDTO = new CategoryResponseDTO();
