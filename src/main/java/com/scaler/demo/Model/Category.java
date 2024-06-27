b999b1c315f83be65d010bb502d24db6e660217a
Mitta Shireesha <mittashireesha@Mittas-Laptop.local>
2024-06-27 23:43:20 +0530

    Fetch Types

--- src/main/java/com/scaler/demo/Model/Category.java
+++ src/main/java/com/scaler/demo/Model/Category.java
@@ -2,10 +2,7 @@ package com.scaler.demo.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.scaler.demo.dto.CategoryResponseDTO;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,7 +18,7 @@ public class Category  extends BaseModel{
 
      private String name;
 
-     @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE) // we give mapped by to avoid duplicate relations
+     @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY) // we give mapped by to avoid duplicate relations
      //else it creates separate mapping table for category product combination
      // as we have already specified cardinality in Product class
      @JsonIgnore
--- src/test/java/com/scaler/demo/DemoApplicationTests.java
+++ src/test/java/com/scaler/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.scaler.demo;
 
+import com.scaler.demo.Model.Category;
+import com.scaler.demo.Model.Product;
+import com.scaler.demo.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@Transactional
 class DemoApplicationTests {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testFetchType(){
+        List<Category> categoryList = categoryRepository.findAll();
+        for (Category category : categoryList) {
+            System.out.println(category);
+        }
+        System.out.println(categoryList.get(0));
+
+        List<Product> products = categoryList.get(0).getProducts();
+        System.out.println(products);
+    }
 }
